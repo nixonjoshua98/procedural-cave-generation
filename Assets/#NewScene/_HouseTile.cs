edfdc61d1ba440908b7153ac12832b2542c0ae05
Joshua <nixonjoshua98@gmail.com>
2019-10-22 17:30:32 +0100

    Updated

---
+++ Assets/#NewScene/_HouseTile.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class _HouseTile : MonoBehaviour
+{
+	static int houseObjIndex = 0;
+
+	public GameObject[] houseOBJs;
+	public void Generate(Vector3 lookAtPos)
+	{
+		GameObject parent = GameObject.FindGameObjectWithTag("DioramaParent");
+
+		GameObject houseSlot = transform.GetChild(0).gameObject;
+
+		GameObject houseToSpawn = houseOBJs[houseObjIndex++ % houseOBJs.Length];
+
+		Vector3 pos = houseSlot.transform.position;
+
+		pos.y = houseToSpawn.transform.localScale.y / 2 + (transform.localScale.y / 2);
+
+		GameObject spawnedHouse = Instantiate(houseToSpawn, pos, houseToSpawn.transform.rotation);
+
+		spawnedHouse.transform.parent = parent.transform;
+
+		Vector3 dir = (lookAtPos - spawnedHouse.transform.position).normalized;
+		Quaternion lookRotation = Quaternion.LookRotation(dir);
+
+		lookRotation.x = 0;
+		lookRotation.z = 0;
+
+		spawnedHouse.transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Random.Range(0.75f, 1.25f));
+	}
+}
---
+++ Assets/#NewScene/_TileGenerator.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class _TileGenerator : MonoBehaviour
+{
+	[Header("Tiles")]
+	public GameObject emptyTileOBJ;
+	public GameObject settlementTileOBJ;
+
+	[Header("Parent Gameobjects")]
+	public GameObject tilesParent;
+
+	public void Generate()
+	{
+		GenerateEmptyTiles();
+
+		GenerateSettlementTiles();
+	}
+
+	private void GenerateEmptyTiles()
+	{
+		int WORLD_SIZE = _Manager.inst.WORLD_SIZE;
+		int TILE_SIZE = _Manager.inst.TILE_SIZE;
+
+		float negativeCoord = -((WORLD_SIZE / 2) * TILE_SIZE) + (WORLD_SIZE % 2 == 0 ? TILE_SIZE / 2 : 0.0f);
+
+		for (int y = 0; y < WORLD_SIZE; y++)
+		{
+			for (int x = 0; x < WORLD_SIZE; x++)
+			{
+				GameObject tileToSpawn = emptyTileOBJ;
+
+				Vector3 pos = new Vector3(negativeCoord + (x * TILE_SIZE), 0, -(negativeCoord + (y * TILE_SIZE)));
+
+				GameObject spawnedTile = Instantiate(tileToSpawn, pos, Quaternion.identity, tilesParent.transform);
+
+				_Manager.inst.allTiles[y * WORLD_SIZE + x] = spawnedTile;
+			}
+		}
+	}
+
+	private void GenerateSettlementTiles()
+	{
+		var allTiles = _Manager.inst.allTiles;
+
+		List<int> potentialIndexes = new List<int>();
+		for (int i = 0; i < allTiles.Length; i++) { potentialIndexes.Add(i); }
+
+		int NUM_SETTLEMENTS = (int)Random.Range(2, Mathf.Min(Mathf.Sqrt(_Manager.inst.WORLD_SIZE), 7));
+
+		for (int i = 0; i < NUM_SETTLEMENTS; i++)
+		{
+			int rand = Random.Range(0, potentialIndexes.Count);
+
+			GameObject emptyTile = allTiles[rand];
+
+			Vector3 spawnPos = emptyTile.transform.position;
+
+			GameObject settlementTile = Instantiate(settlementTileOBJ, spawnPos, emptyTile.transform.rotation);
+
+			settlementTile.transform.parent = tilesParent.transform;
+
+			// Switch tiles and remove the index the settlement will spawn at.
+			Destroy(emptyTile);
+			allTiles[potentialIndexes[rand]] = settlementTile;
+			potentialIndexes.RemoveAt(rand);
+		}
+	}
+}
