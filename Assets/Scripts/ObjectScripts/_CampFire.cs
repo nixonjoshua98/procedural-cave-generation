9852a50a528c8ee85d985d7407497db162d0b953
Joshua <nixonjoshua98@gmail.com>
2019-11-04 09:16:26 +0000

    Updated

---
+++ Assets/Scripts/ObjectScripts/_CampFire.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class _CampFire : MonoBehaviour
+{
+	public GameObject rock;
+	public GameObject logPile;
+
+	private void Start()
+	{
+		Generate();
+	}
+
+
+	public void Generate()
+	{
+		int numRocks	= Random.Range(30, 45);
+		Vector3 center	= transform.position;
+		float radius	= Random.Range(0.85f, 1.0f);
+
+		Quaternion rotation;
+		Vector3 scale;
+		Vector3 pos;
+
+		for (int i = 0; i < numRocks; i++)
+		{
+			float angle		= 360 / numRocks * i;
+			pos				= GetPosition(center, radius, angle);
+			GameObject r	= Instantiate(rock, pos, Quaternion.identity);
+
+			scale		= r.transform.localScale;
+			rotation	= r.transform.rotation;
+
+			scale *= Random.Range(1.0f, 1.5f);
+
+			rotation.z = Random.Range(0, 270.0f);
+
+			r.transform.localScale	= scale;
+			r.transform.rotation	= rotation;
+			r.transform.parent		= transform;
+			
+		}
+
+		GameObject l = Instantiate(logPile, center, Quaternion.identity);
+
+		rotation	= l.transform.rotation;
+		pos			= l.transform.position;
+
+		pos.y		= transform.position.y + (transform.localScale.y / 2) + (rock.transform.localScale.y / 2) + Random.Range(0.25f, 0.3f);
+		rotation.y	= Random.Range(0.0f, 270.0f);
+
+		l.transform.parent		= transform;
+		l.transform.position	= pos;
+		l.transform.rotation	= rotation;
+	}
+
+	Vector3 GetPosition(Vector3 center, float radius, float angle)
+	{
+		Vector3 pos = new Vector3()
+		{
+			x = center.x + radius * Mathf.Sin(angle * Mathf.Deg2Rad),
+			y = transform.position.y + (transform.localScale.y / 2) + (rock.transform.localScale.y / 2) + 0.025f,
+			z = center.z + radius * Mathf.Cos(angle * Mathf.Deg2Rad)
+		};
+
+		return pos;
+	}
+}
--- Assets/Scripts/_DecorationTileGen.cs
+++ Assets/Scripts/_DecorationTileGen.cs
@@ -4,30 +4,62 @@ using UnityEngine;
 
 public class _DecorationTileGen : MonoBehaviour
 {
-	public GameObject decorationTile;
+	public GameObject[] decorationTiles;
+	public GameObject[] decorations;
+
+	private Quaternion[] rotations = 
+	{
+		Quaternion.Euler(0,		0,		0),
+		Quaternion.Euler(0,		90,		0),
+		Quaternion.Euler(0,		180,	0),
+		Quaternion.Euler(0,		270,	0),
+	};
+
 
 	public void Generate(int worldSize, int tileSize, ref GameObject[] tiles)
 	{
-		List<int> emptyTiles = GetEmptyTiles(ref tiles);
-		int numDecorations = (int) (emptyTiles.Count * 0.25f);
+		int decorationTileIndex = 0;
+		List<int> emptyTiles	= GetEmptyTiles(ref tiles);
+		int numDecorations		= (int) (emptyTiles.Count * 0.15f);
 
 		for (int i = 0; i < numDecorations; i++)
 		{
-			int rand = Random.Range(0, emptyTiles.Count);
+			int rand		= Random.Range(0, emptyTiles.Count);
+			int tilesIndex	= emptyTiles[rand];
 
-			int tilesIndex = emptyTiles[rand];
+			GameObject emptyTile	= tiles[tilesIndex];
+			GameObject decoTile		= Instantiate(decorationTiles[decorationTileIndex], emptyTile.transform.position, Quaternion.identity);
 
-			GameObject emptyTile = tiles[tilesIndex];
-
-			GameObject decoTile = Instantiate(decorationTile, emptyTile.transform.position, Quaternion.identity);
-
-			decoTile.transform.parent = emptyTile.transform.parent;
+			decoTile.transform.parent	= emptyTile.transform.parent;
+			decoTile.transform.rotation = rotations[Random.Range(0, rotations.Length)];
 
 			Destroy(emptyTile);
 
 			tiles[tilesIndex] = decoTile;
 
 			emptyTiles.RemoveAt(rand);
+
+			GenerateDecorations(decoTile);
+
+			decorationTileIndex = ++decorationTileIndex % decorationTiles.Length;
+		}
+	}
+
+	private void GenerateDecorations(GameObject tile)
+	{
+		int childCount = tile.transform.childCount;
+
+		for (int i = 0; i < childCount; i++)
+		{
+			GameObject slot = tile.transform.GetChild(i).gameObject;
+
+			GameObject deco;
+
+			deco = Instantiate(decorations[Random.Range(0, decorations.Length)], slot.transform.position, tile.transform.rotation);
+
+			deco.transform.parent = tile.transform;
+
+			Destroy(slot.gameObject);
 		}
 	}
 
