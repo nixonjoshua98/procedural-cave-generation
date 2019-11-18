5f19ae8cf49a410096272f3b4053815d9dc7a61b
Joshua <nixonjoshua98@gmail.com>
2019-11-18 22:44:06 +0000

    Stable

---
+++ New/PCG/Assets/MeshGenerator.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class MeshGenerator
+{
+	static Vector3[] startingVertices = {
+			new Vector3 (-0.5f,     -0.5f,   -0.5f),
+			new Vector3 (+0.5f,     -0.5f,   -0.5f),
+			new Vector3 (+0.5f,     +0.5f,   -0.5f),
+			new Vector3 (-0.5f,     +0.5f,   -0.5f),
+			new Vector3 (-0.5f,     +0.5f,   +0.5f),
+			new Vector3 (+0.5f,     +0.5f,   +0.5f),
+			new Vector3 (+0.5f,     -0.5f,   +0.5f),
+			new Vector3 (-0.5f,     -0.5f,   +0.5f),
+		};
+
+	static int[] startingTriangles = {
+			0, 2, 1, 0, 3, 2,	// Front
+			2, 3, 4, 2, 4, 5,	// Top
+			1, 2, 5, 1, 5, 6,	// Right
+			0, 7, 4, 0, 4, 3,	// Left
+			5, 4, 7, 5, 7, 6,	// Back
+			0, 6, 7, 0, 1, 6	// Bottom
+		};
+
+	public static void DrawCubes(Vector3[] cubePositions, int worldSize, Mesh mesh)
+	{
+
+		int[] triangles = new int[startingTriangles.Length * (worldSize * worldSize)];
+		Vector3[] vertices = new Vector3[startingVertices.Length * (worldSize * worldSize)];
+
+		mesh.Clear();
+
+		for (int i = 0; i < cubePositions.Length; i++)
+		{
+			Vector3 pos = cubePositions[i];
+
+			// Add vertices
+			for (int j = 0; j < startingVertices.Length; j++)
+			{
+				int index = (i * startingVertices.Length) + j;
+				Vector3 v = startingVertices[j] + pos;
+
+				vertices[index] = v;
+			}
+
+			// Add triangles
+			for (int j = 0; j < startingTriangles.Length; j++)
+			{
+				int index = (i * startingTriangles.Length) + j;
+				int value = startingTriangles[j] + (i * startingVertices.Length);
+
+				triangles[index] = value;
+			}
+		}
+
+		mesh.vertices = vertices;
+
+		mesh.triangles = triangles;
+
+		mesh.Optimize();
+
+		mesh.RecalculateNormals();
+	}
+}
---
+++ New/PCG/Assets/Noise.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using LibNoise;
+using LibNoise.Generator;
+using LibNoise.Operator;
+
+public static class Noise
+{
+	public static float[,] GenerateNoiseMap(int worldSize, float scale, double frequency, double lacunarity, double persistance, int octaves)
+	{
+		Perlin perlin = new Perlin(frequency, lacunarity, persistance, octaves, Random.Range(0, worldSize * worldSize), QualityMode.High);
+
+		float[,] map = new float[worldSize, worldSize];
+
+		for (int y = 0; y < worldSize; y++)
+		{
+			for (int x = 0; x < worldSize; x++)
+			{
+				float _x = x / scale;
+				float _y = y / scale;
+
+				map[x, y] = (float) perlin.GetValue(_x, 0.0f, _y);
+			}
+		}
+
+		return map;
+	}
+}
