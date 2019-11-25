77f2c188ea68d96a92aaba958af578c223ab4aeb
Joshua Nixon <nixonjoshua98@gmail.com>
2019-11-25 12:36:04 +0000

    Stable

---
+++ New/PCG/Assets/Scripts/Noise.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using LibNoise;
+using LibNoise.Generator;
+using LibNoise.Operator;
+
+public static class Noise
+{
+	public static float[,] GenerateNoiseMap(int worldWidth, int worldHeight, double frequency, double lacunarity, double persistance, int octaves, int seed)
+    {
+        using (Perlin perlin = new Perlin(frequency, lacunarity, persistance, octaves, seed, QualityMode.High))
+        {
+            float[,] map = new float[worldWidth, worldHeight];
+
+            for (int y = 0; y < worldHeight; y++)
+            {
+                for (int x = 0; x < worldWidth; x++)
+                {
+                    float _x = x;
+                    float _y = y;
+
+                    map[x, y] = (float)perlin.GetValue(_x, 0.0f, _y);
+                }
+            }
+
+            return map;
+        }
+    }
+}
---
+++ New/PCG/Assets/Scripts/TerrainGenerator.cs
@@ -0,0 +1,125 @@
+﻿using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+public class TerrainGenerator : MonoBehaviour
+{
+	[Header("Perlin Noise")]
+	[Range(0.0f, 0.25f)] public double frequency;
+	[Range(0.0f, 1.0f)] public double lacunarity;
+	[Range(0.0f, 2.0f)] public double persistance;
+	[Range(0, 10)]  public int octaves;
+
+	[Header("World Attributes")]
+	[Range(8, 128)] public int tileSize;
+	[Space]
+	[Range(16, 250)] public int worldWidth;
+	[Range(16, 250)] public int worldHeight;
+
+	[Header("Border Attributes")]
+	[Range(1.0f, 64.0f)] public float borderHeightMultiplier;
+	[Range(16, 250)] public int borderSize;
+
+	[Header("Mesh Attributes")]
+	public float meshHeightMultiplier;
+	public AnimationCurve meshHeightCurve;
+
+    [Header("Components")]
+    public MeshFilter meshFilter;
+    public MeshRenderer meshRenderer;
+
+    [Space]
+
+	public TerrainType[] regions;
+
+	private void Start()
+	{
+		GenerateTerrain();
+	}
+
+	public void GenerateTerrain()
+	{
+
+		float[,] noiseMap = Noise.GenerateNoiseMap(worldWidth, worldHeight, frequency, lacunarity, persistance, octaves, Random.Range(-99999, 99999));
+
+		TerrainType[] terrainMap	= GenerateTerrainTypeMap(noiseMap);
+		Color[] colorMap			    = GenerateColorMap(terrainMap);
+
+		Texture2D meshTexture   = TextureFromColorMap(colorMap, worldWidth, worldHeight);
+		MeshData meshData       = MeshGenerator.GenerateTerrainMesh(noiseMap, meshHeightMultiplier, borderSize, borderHeightMultiplier, meshHeightCurve);
+
+		DrawMesh(meshData, meshTexture);
+
+        SettlementGenerator settlementGenerator = GetComponent<SettlementGenerator>();
+
+        settlementGenerator.Generate();
+
+    }
+
+	public TerrainType[] GenerateTerrainTypeMap(float[,] noiseMap)
+	{
+		TerrainType[] terrainMap = new TerrainType[worldHeight * worldWidth];
+
+		for (int y = 0; y < worldHeight; y++)
+		{
+			for (int x = 0; x < worldWidth; x++)
+			{
+				float height = noiseMap[x, y];
+
+				foreach (TerrainType r in regions)
+				{
+					if (height <= r.height)
+					{
+						terrainMap[x + (y * worldWidth)] = r;
+
+						break;
+					}
+				}
+			}
+		}
+
+		return terrainMap;
+	}
+
+	public Color[] GenerateColorMap(TerrainType[] terrainMap)
+	{
+		Color[] colorMap = new Color[worldWidth * worldHeight];
+
+		for (int i = 0; i < terrainMap.Length; i++)
+			colorMap[i] = terrainMap[i].color;
+
+		return colorMap;
+	}
+
+    public static Texture2D TextureFromColorMap(Color[] colorMap, int width, int height)
+    {
+        Texture2D texture = new Texture2D(width, height)
+        {
+            filterMode = FilterMode.Point,
+            wrapMode = TextureWrapMode.Clamp
+        };
+
+        texture.SetPixels(colorMap);
+
+        texture.Apply();
+
+        return texture;
+    }
+
+    public void DrawMesh(MeshData meshData, Texture2D texture)
+    {
+        meshFilter.sharedMesh = meshData.CreateMesh();
+
+        meshRenderer.sharedMaterial.mainTexture = texture;
+    }
+}
+
+[System.Serializable]
+public struct TerrainType
+{
+	public string name;
+	public float height;
+	public Color color;
+}
