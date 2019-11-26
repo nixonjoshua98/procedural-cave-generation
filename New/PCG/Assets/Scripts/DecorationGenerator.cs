09f1ed1737bf774b847d8992f12ef627872187fa
Joshua <nixonjoshua98@gmail.com>
2019-11-26 12:27:20 +0000

    Update DecorationGenerator.cs

--- New/PCG/Assets/Scripts/DecorationGenerator.cs
+++ New/PCG/Assets/Scripts/DecorationGenerator.cs
@@ -6,46 +6,44 @@ public class DecorationGenerator : MonoBehaviour
 {
 	public GameObject house;
 
-    public void Generate(int worldWidth, int worldHeight, TerrainType[] terrainMap, Vector3[] vertices, int tileSize, int borderSize, GameObject parent)
-    {
+	public void Generate(int worldWidth, int worldHeight, TerrainType[] terrainMap, Vector3[] vertices, int tileSize, int borderSize, GameObject parent)
+	{
 		for (int y = 0; y < worldHeight; y++)
 		{
 			for (int x = 0; x < worldWidth; x++)
 			{
-				// Border
-				if (x < borderSize || x > worldWidth - borderSize || y < borderSize || y > worldHeight - borderSize)
-					continue;
-
-				int index = x + (y * worldWidth);
-
+				bool isBorder		= x < borderSize || x > worldWidth - borderSize || y < borderSize || y > worldHeight - borderSize;
+				int index			= x + (y * worldWidth);
 				TerrainType terrain = terrainMap[index];
+				Vector3 v			= vertices[index];
 
-				Vector3 v = vertices[index];
+				v.x = (v.x * tileSize) + (tileSize / 2);
+				v.z = (v.z * tileSize) - (tileSize / 2);
 
-				if (terrain.name == "Grass")
+				GameObject obj	= null;
+				float rand		= Random.value;
+
+				// Border
+				if (isBorder)
 				{
-					v.x = (v.x * tileSize) + (tileSize / 2);
-					v.z = (v.z * tileSize) - (tileSize / 2);
+					continue;
+				}
 
-					float rand = Random.value;
-
-					GameObject obj = null;
-
-					if (rand <= 0.15f)
+				else if (!isBorder)
+				{
+					if (terrain.name == "Grass")
 					{
-						obj = Instantiate(house);
-
-						v.y += obj.transform.localScale.y;
+						if (rand <= 0.15f)
+							obj = Instantiate(house, parent.transform);
 					}
+				}
 
+				if (obj != null)
+				{
+					v.y += 16.0f;
 
-					if (obj != null)
-					{
-						obj.transform.parent = parent.transform;
-
-						obj.transform.position		= v;
-						obj.transform.localScale	= new Vector3(1.0f, 32.0f, 1.0f);
-					}
+					obj.transform.position = v;
+					obj.transform.localScale = new Vector3(1.0f, 32.0f, 1.0f);
 				}
 			}
 		}
